228d3d04f4451f5877e30fc785ecc276993b87ac
Muhammad Tauqeer <taukeer.liaqat@gmail.com>
2020-06-09 14:52:09 +0500

    Update diff.sh

--- diff.sh
+++ diff.sh
@@ -8,7 +8,12 @@ zip_file="${current_branch}.zip"
 #echo $(printf "%q" "$zip_file");
 
 #make diff file
+echo "making diff file..."
 git diff > $diff_file
 
 #make zip file
+echo "making zip file"
 git status --porcelain | grep -e '^??'|sed -e 's/^?? //g'| zip $(printf "%q" "$zip_file") -@ -x $zip_file -x git-diff.sh
+
+echo "removing diff file"
+rm $diff_file
